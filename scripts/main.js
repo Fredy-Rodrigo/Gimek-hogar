2a4910fd5ce520472f31af139c62a107752eb12f
Fredy-Rodrigo <rod.cond.fredy@gmail.com>
2022-10-10 18:57:03 -0500

    sliders a seccion opinion

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/* MENU RESPONSIVE */
 let menuIcon = document.getElementById('menu-icon')
 let menu = document.getElementById('menu')
 
@@ -5,3 +6,40 @@ menuIcon.addEventListener('click', () => {
     window.visualViewport.width<=800 && 
     menu.classList.toggle('menu-off');
 })
+
+/* SLIDER OPINIONES */
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.glider'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+        draggable: true,
+		dots: '.dots',
+		arrows: {
+			prev: '.glider-prev',
+			next: '.glider-next'
+		},
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 650,
+                settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            },{
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            }
+        ]
+	});
+});

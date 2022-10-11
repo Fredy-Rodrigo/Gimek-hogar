7cc87e5b532b0b2e32aec03623589e5e8633a6e9
Fredy-Rodrigo <rod.cond.fredy@gmail.com>
2022-10-11 16:56:28 -0500

    sliders a la seccion ofertas y a las categorias

--- scripts/main.js
+++ scripts/main.js
@@ -80,3 +80,87 @@ window.addEventListener('load', function(){
         ]
 	});
 });
+
+/* SLIDER OFERTAS */
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.glider-offers'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+        draggable: true,
+		dots: '.dots-offers',
+		arrows: {
+			prev: '.glider-offer-prev',
+			next: '.glider-offer-next'
+		},
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 650,
+                settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            },{
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            }
+        ]
+	});
+});
+
+/* SLIDER CATEGORIAS */
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.glider-categories'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+        draggable: true,
+		dots: '.dots-categories',
+		arrows: {
+			prev: '.glider-cat-prev',
+			next: '.glider-cat-next'
+		},
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 0,
+                settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 1.5,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            },{
+                // screens greater than >= 1024px
+                breakpoint: 730,
+                settings: {
+                slidesToShow: 2.5,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            }
+            ,{
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3.5,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            }
+        ]
+	});
+});

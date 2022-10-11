4149ea650474b6864161dcb277da6c33955bc487
Fredy-Rodrigo <rod.cond.fredy@gmail.com>
2022-10-11 15:15:41 -0500

    sliders a seccion productos mas vendidos

--- scripts/main.js
+++ scripts/main.js
@@ -16,8 +16,45 @@ window.addEventListener('load', function(){
         draggable: true,
 		dots: '.dots',
 		arrows: {
-			prev: '.glider-prev',
-			next: '.glider-next'
+			prev: '.glider-opinion-prev',
+			next: '.glider-opinion-next'
+		},
+        responsive: [
+            {
+                // screens greater than >= 775px
+                breakpoint: 650,
+                settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            },{
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+                }
+            }
+        ]
+	});
+});
+
+/* SLIDER PRODUCTOS MAS VENDIDOS */
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.glider-products'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+        draggable: true,
+		dots: '.dots-products',
+		arrows: {
+			prev: '.glider-prod-prev',
+			next: '.glider-prod-next'
 		},
         responsive: [
             {
